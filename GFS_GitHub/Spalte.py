b70f0078955d7ca49f3d56de451ced471734c6f6
Eduard15453 <ediurban22@gmail.com>
2024-06-19 12:18:31 +0200

    Update Spalte.py

--- GFS_GitHub/Spalte.py
+++ GFS_GitHub/Spalte.py
@@ -1,6 +1,12 @@
 from numpy import *
 from MATRIX import a
 
+def TestS(Spieler):
+    for i in range (len(a)):
+        if  a[0][i] == a[1][i] == a[2][i] == Spieler:
+            return True
+        
+
 
 
 #Erstelle die Methode TestS, welche die Spalten überprüft.
