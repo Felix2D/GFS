9acba86dc6c4e1a85e46778e33d319ee220a04e4
GAXNWith100iq <93179452+GAXNWith100iq@users.noreply.github.com>
2024-06-19 12:06:15 +0200

    Update Zeile.py
    Felix RRR

--- GFS_GitHub/Zeile.py
+++ GFS_GitHub/Zeile.py
@@ -1,6 +1,10 @@
 from numpy import *
 from MATRIX import a
-
-
 #Erstelle die Methode TestZ, welche die Zeilen überprüft.
 #Die vom "Spielfeld" übermittelte Variable ist die Spielernummer
+
+def TestZ(Spieler):
+    temp =0
+    for y in range(3):
+        if a[0][y] == a[1][y] == a[2][y] == Spieler:
+            return True
